7c666d17f4b47ea8d46f974f0be731288e1ffc00
WhoAmI-FK <Faismen2004@mail.ru>
2022-08-10 21:45:07 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut x = 10;
+    println!("x is {}",x);
+    x = 20;
+    println!("x is {}",x);
+}
+
+/*
+*   cargo run - to run the program 
+*   cargo build --release - to build release
+*/

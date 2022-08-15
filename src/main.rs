0b9fb88aa7678296d2d6556119d20ec9a01f1015
WhoAmI-FK <Faismen2004@mail.ru>
2022-08-15 21:28:56 +0200

    update refs

--- src/main.rs
+++ src/main.rs
@@ -160,8 +160,32 @@ fn main() {
     }
     println!("outer_planet is {}",outer_planet);
 
+
+    let mut rocket_fuel = String::from("RP-1");
+    let length = process_fuel(&mut rocket_fuel);
+
+    println!("rocket_fuels is {} and length is {}",rocket_fuel,length);
+    
+    let greetings = String::from("Greetings from Earth!");
+    println!("{}",greetings);
+
+    let last_word = &greetings[15..];
+    println!("Slice: {}",last_word);
 }
 
+
+fn process_fuel(propellant: &mut String) -> usize
+{
+    println!("Processing propellant {}...",propellant);
+    propellant.push_str(" is highly flammable!");
+    let length = propellant.len();
+    length
+}
+
+fn produce_fuel() -> String{
+    let new_fuel = String::from("RP-1");
+    new_fuel
+}
 
 fn say_hello() {
     println!("Hello!");
